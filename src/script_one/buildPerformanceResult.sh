dd1f7b72483bb2a8e6e0fa54b358cb234c2809c4
Fabien <tiretfabien@hotmail.fr>
2018-03-21 10:06:02 +0100

    prepull last merge

--- src/script_one/buildPerformanceResult.sh
+++ src/script_one/buildPerformanceResult.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-resultFile=results
 label=$1
+resultFile=results/$1_results
 execFile=src/script_one/buildPerfOneModel.sh
-opt="-w+1 19 -g "
+opt="-w+1 3 -g"
 #echo "" > $resultFile
-for i in `seq 5 9`;
+for i in `seq 1 9`:
 do
-	bash $execFile $label $opt 0.2$i| grep "^0" >> $resultFile
+	bash $execFile $label $opt 0.$i| grep "^0" >> $resultFile
 	echo "execution $i termine"
 done
 
